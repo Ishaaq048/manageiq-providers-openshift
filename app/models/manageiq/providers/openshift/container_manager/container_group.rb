8f071ed50cc6c8442dd705102f139d95eedd98b0
ishaaq.sr <ishaaq.sr@xaasio.com>
2026-07-06 13:49:59 +0530

    Openshift container - pod terminal added

--- app/models/manageiq/providers/openshift/container_manager/container_group.rb
+++ app/models/manageiq/providers/openshift/container_manager/container_group.rb
@@ -1,4 +1,9 @@
 ManageIQ::Providers::Kubernetes::ContainerManager::ContainerGroup.include(ActsAsStiLeafClass)
 
 class ManageIQ::Providers::Openshift::ContainerManager::ContainerGroup < ManageIQ::Providers::Kubernetes::ContainerManager::ContainerGroup
+  private
+  
+  def terminal_binary
+    "oc"
+  end
 end
